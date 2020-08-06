f77e439e360f7ec0c3434f05f4983f692d2a9785
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-08-06 05:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function findMatching(drivers, name) {
   return drivers.filter(e => e.toLowerCase() == name.toLowerCase())
 }
+
+function fuzzyMatch(drivers, letter) {
+  return driver.filter(e => e[0].toLowerCase() = letter.toLowerCase())
+}
